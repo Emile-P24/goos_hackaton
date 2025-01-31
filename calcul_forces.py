0e090c67fd27ca111e0704fe3b01f112785787e9
Elyes Beloucif <elyes.beloucif@etu.minesparis.psl.eu>
2025-01-31 10:25:25 +0100

    ajout dynamique

--- calcul_forces.py
+++ calcul_forces.py
@@ -7,6 +7,7 @@ import pymunk
 class Goo:
     def __init__(self, position):
         self.position = np.array(position, dtype=float)
+        self.velocity = np.array([0.0, 0.0])
         self.mass = 0.4  #en kg
         self.radius = 0.01  #en m
         self.links = []  #connexions (Goos ou plateformes)
@@ -23,6 +24,7 @@ class Goo:
         total_force = np.array([0.0, 0.0])
         gravity = np.array([0, -9.81 / 20]) * self.mass  # Gravité réduite
         total_force += gravity
+
         for other in self.links:
             displacement = self.position - other.position
             distance = np.linalg.norm(displacement)
@@ -30,7 +32,14 @@ class Goo:
             force_magnitude = self.k * (distance - rest_length)
             force_direction = displacement / distance if distance != 0 else np.array([0, 0])
             total_force += force_magnitude * force_direction
+
         return total_force
+    
+    def update(self, dt):
+        force = self.compute_force()
+        acceleration = force / self.mass
+        self.velocity += acceleration * dt
+        self.position += self.velocity * dt
     
 class Platform:
     def __init__(self, points):
@@ -45,3 +54,27 @@ class Platform:
                 min_dist = dist
                 closest = p
         return closest
+    
+def simulate(goos, dt=0.01, steps=1000):
+    for _ in range(steps):
+        for goo in goos:
+            goo.update(dt)
+
+# Exemple d'utilisation
+platform1 = Platform([(0, 0), (5, 0)])
+platform2 = Platform([(10, 0), (15, 0)])
+
+goo1 = Goo((2, 1))
+goo2 = Goo((3, 1))
+goo3 = Goo((4, 2))
+
+goo1.attach(goo2)
+goo2.attach(goo3)
+
+goos = [goo1, goo2, goo3]
+
+simulate(goos)
+
+# Affichage des positions finales
+for goo in goos:
+    print(f"Position d'équilibre du Goo: {goo.position}")
